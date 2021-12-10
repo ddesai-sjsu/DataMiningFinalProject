23cfcc8979c326708a2e61cdb110ae0375347852
ddesai-sjsu <deesha.desai@sjsu.edu>
2021-12-09 20:39:51 -0800

    changes

--- H1B_Prediction_App/h1bprediction/h1bpredictor/views.py
+++ H1B_Prediction_App/h1bprediction/h1bpredictor/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import *
+import numpy as np
 
 # Create your views here.
 
@@ -15,22 +16,29 @@ import pickle
 #        'Total_Wage']
 
 
-def getPrediction(listInput):
+def getPrediction(AMENDED_PETITION, CHANGE_EMPLOYER, CHANGE_PREVIOUS_EMPLOYMENT,
+                  CONTINUED_EMPLOYMENT,EMPLOYER_NAME, EMPLOYER_STATE,
+       FULL_TIME_POSITION, H1B_DEPENDENT, NAICS_CODE,
+       NEW_CONCURRENT_EMPLOYMENT, NEW_EMPLOYMENT, SOC_CODE,
+       SUPPORT_H1B, Total_Wage, WILLFUL_VIOLATOR, WORKSITE_STATE):
 
-    transformed_EmpName = settings.EMP_NAME_LE.transform([listInput[4]])
-    transformed_EmpState = settings.EMP_STATE_LE.transform([listInput[5]])
-    transformed_NaicsCode = settings.NAICS_CODE_LE.transform([listInput[8]])
-    transformed_SocCode = settings.SOC_CODE_LE.transform([listInput[11]])
-    transformed_WorkState = settings.WORKSTATE_LE.transform([listInput[15]])
+    transformed_EmpName = settings.EMP_NAME_LE.transform([EMPLOYER_NAME])
+    transformed_EmpState = settings.EMP_STATE_LE.transform([EMPLOYER_STATE])
+    transformed_NaicsCode = settings.NAICS_CODE_LE.transform([NAICS_CODE])
+    transformed_SocCode = settings.SOC_CODE_LE.transform([SOC_CODE])
+    transformed_WorkState = settings.WORKSTATE_LE.transform([WORKSITE_STATE])
 
-    listInput[4] = transformed_EmpName
-    listInput[5] = transformed_EmpState
-    listInput[8] = transformed_SocCode
-    listInput[11] = transformed_NaicsCode
-    listInput[15] = transformed_WorkState
-    print(listInput)
+    EMPLOYER_NAME = transformed_EmpName
+    EMPLOYER_STATE = transformed_EmpState
+    SOC_CODE = transformed_SocCode
+    NAICS_CODE = transformed_NaicsCode
+    WORKSITE_STATE = transformed_WorkState
 
-    prediction = settings.NN_MODEL.predict([listInput])
+    single=np.array([[AMENDED_PETITION, CHANGE_EMPLOYER, CHANGE_PREVIOUS_EMPLOYMENT, CONTINUED_EMPLOYMENT, EMPLOYER_NAME,
+      EMPLOYER_STATE, FULL_TIME_POSITION, H1B_DEPENDENT, NAICS_CODE, NEW_CONCURRENT_EMPLOYMENT, NEW_EMPLOYMENT,
+      SOC_CODE, SUPPORT_H1B, Total_Wage, WILLFUL_VIOLATOR, WORKSITE_STATE]])
+
+    prediction = settings.NN_MODEL.predict(single)
     return prediction
 
 
@@ -58,11 +66,22 @@ def index(request):
             # model.predit()
             print([EMPLOYER_NAME, EMPLOYER_STATE, SOC_CODE, NAICS_CODE, NEW_EMPLOYMENT, CONTINUED_EMPLOYMENT,CHANGE_PREVIOUS_EMPLOYMENT, NEW_CONCURRENT_EMPLOYMENT,CHANGE_EMPLOYER, AMENDED_PETITION, FULL_TIME_POSITION, H1B_DEPENDENT, WORKSITE_STATE, WILLFUL_VIOLATOR, SUPPORT_H1B,Total_Wage])
 
-            prediction = getPrediction([AMENDED_PETITION, CHANGE_EMPLOYER, CHANGE_PREVIOUS_EMPLOYMENT,
-             CONTINUED_EMPLOYMENT, EMPLOYER_NAME, EMPLOYER_STATE,
-             FULL_TIME_POSITION, H1B_DEPENDENT, NAICS_CODE,
-             NEW_CONCURRENT_EMPLOYMENT, NEW_EMPLOYMENT, SOC_CODE,
-             SUPPORT_H1B, Total_Wage, WILLFUL_VIOLATOR, WORKSITE_STATE])
+            prediction = getPrediction(AMENDED_PETITION=AMENDED_PETITION,
+                                       CHANGE_EMPLOYER=CHANGE_EMPLOYER,
+                                       CHANGE_PREVIOUS_EMPLOYMENT=CHANGE_PREVIOUS_EMPLOYMENT,
+                                       CONTINUED_EMPLOYMENT=CONTINUED_EMPLOYMENT,
+                                       EMPLOYER_NAME=EMPLOYER_NAME,
+                                       EMPLOYER_STATE=EMPLOYER_STATE,
+                                       FULL_TIME_POSITION=FULL_TIME_POSITION,
+                                       H1B_DEPENDENT=H1B_DEPENDENT,
+                                       NAICS_CODE=NAICS_CODE,
+                                       NEW_CONCURRENT_EMPLOYMENT=NEW_CONCURRENT_EMPLOYMENT,
+                                       NEW_EMPLOYMENT=NEW_EMPLOYMENT,
+                                       SOC_CODE=SOC_CODE,
+                                       SUPPORT_H1B=SUPPORT_H1B,
+                                       Total_Wage=Total_Wage,
+                                       WILLFUL_VIOLATOR=WILLFUL_VIOLATOR,
+                                       WORKSITE_STATE=WORKSITE_STATE)
             print('Predictionnnnnnnnnn', prediction)
         # return JsonResponse({'success': True})
             if prediction[0]==0:
